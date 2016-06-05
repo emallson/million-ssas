587817f61bfb07863e228a8c1ea7327182400e7b
J David Smith <emallson@archlinux.us>
2016-06-05 11:19:27 -0400

    Switched to PFDS queues and sets

--- chez/on2/infmax/ctvm/traversal.scm
+++ chez/on2/infmax/ctvm/traversal.scm
@@ -1,23 +1,30 @@
 (library (on2 infmax ctvm traversal (1))
   (export dfs bfs)
-  (import (rnrs)
+  (import (scheme)
           (on2 infmax ctvm graph)
           (only (srfi :1 lists) lset-union)
           (only (srfi :26) cut)
-          (srfi :117 list-queues))
+          (pfds queues)
+          (pfds sets))
+
+  (define (any< a b)
+    "< dispatching on the type of a"
+    (cond
+     [(symbol? a) (string<? (symbol->string a) (symbol->string b))]
+     [(string? a) (string<? a b)]
+     [(number? a) (< a b)]))
 
   (define (bfs g v type explore?)
-    (do ([queue (list-queue v) queue]
-         [current v (unless (list-queue-empty? queue)
-                      (list-queue-front queue))]
-         [seen (list v) (if (list-queue-empty? queue)
-                            seen (cons (list-queue-front queue) seen))])
-        ((list-queue-empty? queue) seen)
-      (for-each (cut list-queue-add-back! queue <>)
-                (filter
-                 (lambda (node) (not (member node seen)))
-                 (filter-edges g current type explore?)))
-      (list-queue-remove-front! queue)))
+    (do ([queue (enqueue (make-queue) v) (let-values ([(head remaining) (dequeue queue)])
+                                           (fold-left enqueue
+                                                      remaining
+                                                      (filter (lambda (n) (not (set-member? seen n)))
+                                                              (filter-edges g head type explore?))))]
+         [seen (set-insert (make-set any<) v) (if (queue-empty? queue)
+                                                  seen
+                                                  (let-values ([(head _) (dequeue queue)])
+                                                    (set-insert seen head)))])
+        ((queue-empty? queue) seen)))
 
   (define dfs
     (case-lambda
