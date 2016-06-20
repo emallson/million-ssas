66de5579e8348a86c0387c47d1a5dfd835e2bf31
J David Smith <emallson@atlanis.net>
2016-06-20 16:01:31 -0400

    Much work. Graph implementation works.
    It may be unnecessary to implement my own Graph, but its been a good
    learning experience. I know I at least needed to implement my own BFS
    iterator to support the activator callback.

---
+++ rust/src/graph.rs
@@ -0,0 +1,397 @@
+#[cfg(test)]
+extern crate quickcheck;
+#[cfg(test)]
+use self::quickcheck::{Arbitrary, Gen};
+
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+#[derive(PartialEq, Eq, Hash, Clone, Debug)]
+pub struct Node(u64);
+
+#[cfg(test)]
+impl Arbitrary for Node {
+    fn arbitrary<G: Gen>(g: &mut G) -> Self {
+        Node(u64::arbitrary(g))
+    }
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct Weight(f32);
+
+#[cfg(test)]
+impl Arbitrary for Weight {
+    fn arbitrary<G: Gen>(g: &mut G) -> Self {
+        Weight(f32::arbitrary(g))
+    }
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct Edge {
+    pub from: Node,
+    pub to: Node,
+    pub weight: Weight
+}
+
+#[cfg(test)]
+impl Arbitrary for Edge {
+    fn arbitrary<G: Gen>(g: &mut G) -> Self {
+        Edge {
+            from: Node::arbitrary(g),
+            to: Node::arbitrary(g),
+            weight: Weight::arbitrary(g)
+        }
+    }
+}
+
+/// Describes a basic weighted, directed graph.
+///
+/// Implementations of this trait are free to add additional
+/// functionality. See `CTVMGraph` for an example.
+pub trait Graphlike {
+    /// Returns the number of nodes in the graph.
+    fn count_nodes(&self) -> usize;
+    /// Returns the number of edges in the graph.
+    fn count_edges(&self) -> usize;
+
+    /// Returns the complete node list.
+    fn nodes(&self) -> &HashSet<Node>;
+    /// Returns the out-neighbors of a node.
+    fn neighbors(&self, node: &Node) -> Option<&Vec<Edge>>;
+    /// Returns the in-neighbors of a node.
+    /// # Implementation Note
+    /// This should not involve a scan of all node's edge lists.
+    fn in_neighbors(&self, node: &Node) -> Option<&Vec<Edge>>;
+
+    /// Add a node to the graph. Duplicates aren't allowed. Returns
+    /// `True` if the node was added, `False` if it was already
+    /// present.
+    fn add_node(&mut self, node: Node) ;
+    /// Add an edge to the graph. Duplicate edges are *not* checked
+    /// for, but technically allowed.
+    fn add_edge(&mut self, edge: Edge);
+    /// Remove an edge from the graph. If multiple edges are present,
+    /// remove exactly one arbitrarily. Returns `True` if an edge was
+    /// removed.
+    fn del_edge(&mut self, from: Node, to: Node) -> bool;
+
+    /// Returns the weights of each edge (from, to). Typically will be
+    /// just 0 or 1 edge.
+    fn edge_weights(&self, from: &Node, to: &Node) -> Option<Vec<&Weight>>;
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct Cost(f32);
+
+#[cfg(test)]
+impl Arbitrary for Cost {
+    fn arbitrary<G: Gen>(g: &mut G) -> Self {
+        Cost(f32::arbitrary(g))
+    }
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct Benefit(f32);
+
+#[cfg(test)]
+impl Arbitrary for Benefit {
+    fn arbitrary<G: Gen>(g: &mut G) -> Self {
+        Benefit(f32::arbitrary(g))
+    }
+}
+
+/// A graph specialized for the Cost-Aware Targeted Viral Marketing
+/// problem.
+#[derive(PartialEq, Clone, Debug)]
+pub struct CTVMGraph {
+    nodes: HashSet<Node>,
+    edges: HashMap<Node, Vec<Edge>>,
+    in_edges: HashMap<Node, Vec<Edge>>,
+    num_edges: usize,
+    costs: HashMap<Node, Cost>,
+    benefits: HashMap<Node, Benefit>,
+}
+
+impl Graphlike for CTVMGraph {
+    fn count_nodes(&self) -> usize {
+        self.nodes.len()
+    }
+
+    fn count_edges(&self) -> usize {
+        self.num_edges
+    }
+
+    fn nodes(&self) -> &HashSet<Node> {
+        &self.nodes
+    }
+
+    fn neighbors(&self, node: &Node) -> Option<&Vec<Edge>> {
+        self.edges.get(node)
+    }
+
+    fn in_neighbors(&self, node: &Node) -> Option<&Vec<Edge>> {
+        self.in_edges.get(node)
+    }
+
+    fn add_node(&mut self, node: Node) {
+        self.nodes.insert(node);
+    }
+
+    fn add_edge(&mut self, edge: Edge) {
+        let Edge {ref from, ref to, ..} = edge;
+        assert!(self.nodes.contains(from));
+        assert!(self.nodes.contains(to));
+
+        if self.edges.contains_key(from) {
+            self.edges.get_mut(from).unwrap().push(edge.clone());
+        } else {
+            self.edges.insert(from.clone(), vec![edge.clone()]);
+        }
+
+        if self.in_edges.contains_key(to) {
+            self.in_edges.get_mut(to).unwrap().push(edge.clone());
+        } else {
+            self.in_edges.insert(to.clone(), vec![edge.clone()]);
+        }
+
+        self.num_edges += 1;
+    }
+
+    fn del_edge(&mut self, from: Node, to: Node) -> bool {
+        let deleted_out = match self.edges.get_mut(&from) {
+            Some(vec) => match vec.iter().position(|&Edge {to: ref node, ..}| *node == to) {
+                Some(index) => {
+                    vec.swap_remove(index);
+                    true
+                },
+                None => false
+            },
+            None => false
+        };
+
+        let deleted_in = match self.in_edges.get_mut(&to) {
+            Some(vec) => match vec.iter().position(|&Edge {from: ref node, ..}| *node == from) {
+                Some(index) => {
+                    vec.swap_remove(index);
+                    true
+                },
+                None => false
+            },
+            None => false
+        };
+
+        if deleted_out && deleted_in {
+            self.num_edges -= 1;
+        };
+
+        deleted_out && deleted_in
+    }
+
+    fn edge_weights(&self, from: &Node, to: &Node) -> Option<Vec<&Weight>> {
+        match self.edges.get(&from) {
+            Some(vec) => Some(vec.iter().filter_map(
+                |&Edge {to: ref node, ref weight, ..}|
+                if node == to {
+                    Some(weight)
+                } else {
+                    None
+                }).collect()),
+            None => None
+        }
+    }
+}
+
+impl CTVMGraph {
+
+    fn new() -> CTVMGraph {
+        return CTVMGraph {
+            nodes: HashSet::new(),
+            edges: HashMap::new(),
+            in_edges: HashMap::new(),
+            costs: HashMap::new(),
+            benefits: HashMap::new(),
+            num_edges: 0
+        }
+    }
+
+    fn add_weighted_node(&mut self, node: Node, cost: Cost, benefit: Benefit) {
+        self.add_node(node.clone());
+        self.costs.insert(node.clone(), cost);
+        self.benefits.insert(node.clone(), benefit);
+    }
+
+    fn get_cost(&self, node: &Node) -> Option<&Cost> {
+        self.costs.get(node)
+    }
+
+    fn get_benefit(&self, node: &Node) -> Option<&Benefit> {
+        self.benefits.get(node)
+    }
+}
+
+#[cfg(test)]
+impl Arbitrary for CTVMGraph {
+
+    fn arbitrary<G: Gen>(gen: &mut G) -> Self {
+        let edges = Vec::<Edge>::arbitrary(gen);
+        let mut g = CTVMGraph::new();
+        let nodes = edges.iter().flat_map(|&Edge { ref from, ref to, ..}|
+                                          vec![from.clone(), to.clone()])
+            .collect::<HashSet<Node>>();
+
+        for n in nodes {
+            g.add_weighted_node(n, Cost::arbitrary(gen), Benefit::arbitrary(gen));
+        }
+
+        for e in edges {
+            g.add_edge(e)
+        }
+
+        return g;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::quickcheck::{quickcheck, TestResult};
+    use std::collections::HashSet;
+    use super::*;
+
+    // Tests in this module use CTVMGraph as a concrete way to test
+    // the trait Graphlike because there isn't another concrete impl.
+
+    #[test]
+    fn node_eq() {
+        fn neq(a: Node, b: Node) -> bool {
+            let Node(ac) = a;
+            let Node(bc) = b;
+            a == b || ac != bc
+        }
+
+        quickcheck(neq as fn(Node, Node) -> bool);
+    }
+
+    #[test]
+    fn edge_eq() {
+        fn eq(a: Edge, b: Edge) -> bool {
+            let &Edge {from: ref af, to: ref at, weight: ref aw} = &a;
+            let &Edge {from: ref bf, to: ref bt, weight: ref bw} = &b;
+            a == b || af != bf || at != bt || aw != bw
+        }
+
+        quickcheck(eq as fn(Edge, Edge) -> bool);
+    }
+
+    #[test]
+    fn graph_add_nodes() {
+        fn added_nodes_are_in_list(nodes: HashSet<Node>) -> bool {
+            let mut g = CTVMGraph::new();
+
+            for v in nodes.iter().cloned() {
+                g.add_node(v);
+            }
+
+            g.nodes().iter().cloned().collect::<HashSet<Node>>() == nodes
+        }
+
+        quickcheck(added_nodes_are_in_list as fn(HashSet<Node>) -> bool);
+    }
+
+    #[test]
+    fn graph_add_edges() {
+        fn added_edges_are_in_neighbors(edges: Vec<Edge>) -> bool {
+            let nodes = edges.iter().flat_map(|&Edge { ref from, ref to, ..}|
+                                              vec![from.clone(), to.clone()])
+                .collect::<HashSet<Node>>();
+            let mut g = CTVMGraph::new();
+
+            for v in nodes {
+                g.add_node(v);
+            }
+
+            for e in edges.iter().cloned() {
+                g.add_edge(e);
+            }
+
+            edges.iter().all(|ref e| g.neighbors(&e.from).unwrap().contains(&e)) &&
+                edges.iter().all(|ref e| g.in_neighbors(&e.to).unwrap().contains(&e))
+        }
+        quickcheck(added_edges_are_in_neighbors as fn(Vec<Edge>) -> bool);
+    }
+
+    #[test]
+    fn graph_edge_weight() {
+        fn edge_weight_correct_edge(edges: Vec<Edge>) -> bool {
+            let nodes = edges.iter().flat_map(|&Edge { ref from, ref to, ..}|
+                                              vec![from.clone(), to.clone()])
+                .collect::<HashSet<Node>>();
+            let mut g = CTVMGraph::new();
+
+            for v in nodes {
+                g.add_node(v);
+            }
+
+            for e in edges.iter().cloned() {
+                g.add_edge(e);
+            }
+
+            edges.iter().all(|&Edge { ref from, ref to, ref weight }|
+                             g.edge_weights(from, to).unwrap().contains(&weight))
+        }
+        quickcheck(edge_weight_correct_edge as fn(Vec<Edge>) -> bool);
+    }
+
+    #[test]
+    fn graph_counts() {
+        fn correct_count_nodes(nodes: HashSet<Node>) -> bool {
+            let mut g = CTVMGraph::new();
+
+            for v in nodes.iter().cloned() {
+                g.add_node(v);
+            }
+
+            g.count_nodes() == nodes.len()
+        }
+
+        fn correct_count_edges(edges: Vec<Edge>) -> bool {
+            let nodes = edges.iter().flat_map(|&Edge { ref from, ref to, ..}|
+                                              vec![from.clone(), to.clone()])
+                .collect::<HashSet<Node>>();
+            let mut g = CTVMGraph::new();
+
+            for v in nodes {
+                g.add_node(v);
+            }
+
+            for e in edges.iter().cloned() {
+                g.add_edge(e);
+            }
+
+            g.count_edges() == edges.len()
+        }
+
+        quickcheck(correct_count_nodes as fn(HashSet<Node>) -> bool);
+        quickcheck(correct_count_edges as fn(Vec<Edge>) -> bool);
+    }
+
+    #[test]
+    fn graph_ctvm_weights() {
+        fn correct_weights(nodes: HashSet<Node>, costs: Vec<Cost>, benefits: Vec<Benefit>) -> TestResult {
+            if nodes.len() > costs.len() || nodes.len() > benefits.len() {
+                return TestResult::discard()
+            }
+
+            let mut g = CTVMGraph::new();
+            let vnodes = nodes.iter().collect::<Vec<&Node>>();
+
+            for i in 0..nodes.len() {
+                g.add_weighted_node(vnodes[i].clone(), costs[i].clone(), benefits[i].clone());
+            }
+
+            TestResult::from_bool((0..nodes.len()).all(|i| *g.get_cost(vnodes[i]).unwrap() == costs[i] &&
+                                                       *g.get_benefit(vnodes[i]).unwrap() == benefits[i]))
+        }
+
+        quickcheck(correct_weights as fn(HashSet<Node>, Vec<Cost>, Vec<Benefit>) -> TestResult)
+    }
+}
--- rust/src/ris.rs
+++ rust/src/ris.rs
@@ -1,66 +1,41 @@
-extern crate petgraph;
-extern crate rand;
+use graph::{Graphlike, Node, Edge};
+use std::collections::VecDeque;
 
-use petgraph::{Graph, Bfs, Directed, EdgeType, EdgeDirection};
-use petgraph::visit::{Reversed, NeighborIter, Graphlike, Visitable};
-use petgraph::graph::{NodeIndex, IndexType, Neighbors, Edges, Edge};
-
-use rand::{thread_rng, Rng};
-
-use std::collections::HashSet;
-use std::hash::Hash;
-use std::cmp::Eq;
-
-pub struct IC<G>(pub G);
-
-impl <'a, G: Graphlike> Graphlike for IC<&'a G>
+pub struct ReverseBFS<'a, F: 'a>
 {
-    type NodeId = G::NodeId;
+    queue: VecDeque<&'a Node>,
+    graph: &'a Graphlike,
+    activator: &'a F,
 }
 
-impl <'a, 'b, N, Ty, Ix> NeighborIter<'a> for IC<&'b Graph<N, f32, Ty, Ix>> where
-    Ty: EdgeType,
-    Ix: IndexType,
-{
-    type Iter = Neighbors<'a, f32, Ix>;
-
-    fn neighbors(&'a self, n: NodeIndex<Ix>) -> Self::Iter
+impl<'a, F> ReverseBFS<'a, F> {
+    pub fn new(graph: &'a Graphlike, start: &'a Node, activator: &'a F) -> ReverseBFS<'a, F>
+        where for<'r> F: Fn(&'r Edge) -> bool
     {
-        let existing_edges = self.0.edges_directed(n, EdgeDirection::Incoming)
-            .filter_map(| pair | {
-                let (node, weight) = pair;
-                if thread_rng().gen::<f32>() <= *weight {
-                    Some(self.0.find_edge(node, n))
-                } else {
-                    None
-                }
-            }).collect::<Vec<Edge<_, Ix>>>();
+        let mut q = VecDeque::new();
+        q.push_back(start);
 
-        Neighbors {
-            iter: Edges {
-                skip_start: n,
-                edges: existing_edges,
-                next: [existing_edges.peek(); 2]
+        ReverseBFS { queue: q, graph: graph, activator: activator }
+    }
+
+}
+
+impl<'a, F> Iterator for ReverseBFS<'a, F>
+    where for<'r> F: Fn(&'r &Edge) -> bool
+{
+    type Item = &'a Node;
+
+    fn next(&mut self) -> Option<&'a Node> {
+        let next_node = self.queue.pop_front();
+
+        if let Some(node) = next_node {
+            if let Some(neighbors) = self.graph.in_neighbors(node) {
+                for &Edge {ref from, ..} in neighbors.iter().filter(self.activator) {
+                    self.queue.push_back(from);
+                }
             }
         }
-    }
-}
 
-impl <'a, V: Visitable> Visitable for IC<&'a V> {
-    type Map = V::Map;
-    fn visit_map(&self) -> V::Map {
-        self.0.visit_map()
+        next_node
     }
-}
-
-pub fn sample<'a, G: Graphlike + Visitable + NeighborIter<'a>>(g: &'a G, s: G::NodeId) -> HashSet<G::NodeId> where
-    G::NodeId: Hash + Eq,
-{
-    let mut bfs = Bfs::new(g, s);
-    let mut set = HashSet::new();
-
-    while let Some(u) = bfs.next(g) {
-        set.insert(u);
-    }
-    return set;
 }
